523c1428f45660747441899dee2d779b39bb1ca3
Mehmet Ugur Satir <musatir@gmail.com>
2023-09-18 17:05:40 +0200

    nodemailer values entered as .env vars

--- controllers/sendEmail.js
+++ controllers/sendEmail.js
@@ -1,5 +1,19 @@
+const nodemailer = require("nodemailer");
+
 
 const sendEmail = async (req,res)=>{
+    const testAccount = await nodemailer.createTestAccount();
+
+    const transporter = nodemailer.createTransport({
+        host: process.env.ETHEREAL_HOST,
+        port: process.env.ETHEREAL_PORT,
+        auth: {
+            user: process.env.ETHEREAL_USER,
+            pass: process.env.ETHEREAL_PASS,
+        }
+    });
+
+
     res.send('send email func')
 }
 
